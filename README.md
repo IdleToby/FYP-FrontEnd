18086496cbcba51a29d3a2f2ae4345ebfd2cc6fd
Yan Zhihao <68549369+IdleToby@users.noreply.github.com>
2023-09-29 01:46:19 +0800

    Init project by installing dependencies
    初始化项目，安装依赖

---
+++ README.md
@@ -0,0 +1,2 @@
+# FYP-FrontEnd
+这是FYP的前台项目，使用了vue3, element-plus, tailwind
---
+++ src/views/AboutView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1 className="text-2xl font-bold underline">
+      Hello world!
+    </h1>
+    <el-button type="danger">aaa</el-button>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
